f2204a13baeaeebd3d41d1a04880d7855b7d022f
mawia <92118320+39LPeter@users.noreply.github.com>
2024-05-05 05:21:40 +0300

    Update test_state.py

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python3
-""" tests the state class"""
-import unittest
-from models.base_model import BaseModel
+""" """
+import os
+
+from tests.test_models.test_base_model import TestBasemodel
 from models.state import State
-import pep8
 
 
-class Test_pep8(unittest.TestCase):
-    """pep8 test cases class"""
-    def test_pep8_conformance(self):
-        """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/state.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-
-class TestState(unittest.TestCase):
-    """test State module"""
-    def test_class(self):
-        """test class"""
-        self.assertEqual(State.name, "")
-        self.assertTrue(State, BaseModel)
-
+class TestState(TestBasemodel):
+    """Represents the tests for the State model."""
     def __init__(self, *args, **kwargs):
-        """ Inicialization """
+        """Initializes the test class."""
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ tests name """
+        """Tests the type of name."""
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(
+            type(new.name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
