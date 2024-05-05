f188c014eb25b21fdf070925119fdbd51fdea2dc
mawia <92118320+39LPeter@users.noreply.github.com>
2024-05-05 05:22:59 +0300

    Update test_user.py

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,46 +1,48 @@
 #!/usr/bin/python3
-""" tests the user class"""
-import unittest
-from models.base_model import BaseModel
+""" """
+import os
+from sqlalchemy import Column
+
+from tests.test_models.test_base_model import TestBasemodel
 from models.user import User
-import pep8
 
 
-class Test_pep8(unittest.TestCase):
-    """pep8 test cases class"""
-    def test_pep8_conformance(self):
-        """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/user.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-
-class test_User(BaseModel):
-    """ tests the user class """
-
+class TestUser(TestBasemodel):
+    """Represents the tests for the User model."""
     def __init__(self, *args, **kwargs):
-        """ inicialization """
+        """Initializes the test class."""
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     def test_first_name(self):
-        """ tests the first name """
+        """Tests the type of first_name."""
         new = self.value()
-        self.assertEqual(type(new.first_name), str)
+        self.assertEqual(
+            type(new.first_name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
 
     def test_last_name(self):
-        """ tests the last name """
+        """Tests the type of last_name."""
         new = self.value()
-        self.assertEqual(type(new.last_name), str)
+        self.assertEqual(
+            type(new.last_name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
 
     def test_email(self):
-        """ tests email """
+        """Tests the type of email."""
         new = self.value()
-        self.assertEqual(type(new.email), str)
+        self.assertEqual(
+            type(new.email),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
 
     def test_password(self):
-        """ tests password """
+        """Tests the type of password."""
         new = self.value()
-        self.assertEqual(type(new.password), str)
+        self.assertEqual(
+            type(new.password),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
